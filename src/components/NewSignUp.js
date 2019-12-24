a1420d594234ba05c6af4d369509866af0b88a01
Chris Oakes <chris.oakes@outlook.com>
2019-12-24 12:06:23 -0800

    added form on signup page

--- src/components/NewSignUp.js
+++ src/components/NewSignUp.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Background from "../images/backhandsblur.jpg";
 import styled from "styled-components";
+import { useForm } from 'react-hook-form'
 
 const WrapBackDiv = styled.div`
   height: 100vh;
@@ -15,13 +16,36 @@ const Modal = styled.div`
     width: 80%; 
     border: white 1px solid;
     margin:  auto;
+    font-size: 3.5rem;
+    background: white; 
+`
+const FormStyle = styled.form`
+    display: flex;
+    flex-direction:column;
+    width: 70%;
+
 `
 
+
 const NewSignUp = () => {
+  const { register, handleSubmit } = useForm()
+
   return (
     <>
       <WrapBackDiv>
-         <Modal> Hello from new signup</Modal>
+         <Modal> Join the Conversation
+             <FormStyle >
+                <label htmlFor="name">Name</label>
+                <input label='name'name="name" ref={register} />
+                <label htmlFor="email">Email</label>
+                <input name="email" ref={register} />
+                <label htmlFor="password">Password</label>
+                <input name="password" ref={register} />
+                <label htmlFor="confirm">Confirm</label>
+                <input name="confirm" ref={register} />
+             </FormStyle>
+         </Modal>
+
     </WrapBackDiv>
     </>
   );
