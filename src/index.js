57f37c7d223d03ff9205a20f53d34c597419c6fc
Code4Blessings <robinwarden@code4blessings.com>
2020-01-08 05:22:45 -0500

    Added redux-thunk as a dependency

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import App from './App';
 import { reducer } from './reducer/index';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
